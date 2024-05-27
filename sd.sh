88e67702245fa9872c6c55546e400d6c191ee3b6
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-05-27 14:37:07 -0400

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install pyngrok
+# Install pyngrok if not already installed
 pip install pyngrok
 
 # Prompt user for ngrok authentication token
@@ -16,10 +16,13 @@ read -p "Enter your choice (1 or 2): " device_option
 start_webui() {
     device=$1
     cd /home/studio-lab-user/stable-diffusion-webui
-    if [ "$device" == "cpu" ]; then
+    if [ "$device" = "cpu" ]; then
         python launch.py --port 7860 --skip-torch-cuda-test
-    elif [ "$device" == "gpu" ]; then
+    elif [ "$device" = "gpu" ]; then
         python launch.py --port 7860
+    else
+        echo "Invalid device option."
+        exit 1
     fi
 }
 
@@ -30,20 +33,17 @@ import os
 from pyngrok import ngrok
 
 ngrok.set_auth_token('$ngrok_auth_token')
-public_url = ngrok.connect(7860)
+public_url = ngrok.connect(7860, 'http')
 print(public_url)
 ")
     echo "Starting ngrok tunnel: $public_url"
 }
 
-# Set ngrok authentication token
-python -c "from pyngrok import ngrok; ngrok.set_auth_token('$ngrok_auth_token')"
-
 # Start the Web UI and ngrok based on device option
-if [ "$device_option" == "1" ]; then
+if [ "$device_option" = "1" ]; then
     start_webui "cpu" &
     start_ngrok
-elif [ "$device_option" == "2" ]; then
+elif [ "$device_option" = "2" ]; then
     start_webui "gpu" &
     start_ngrok
 else
